2d4399df7f1c15602b849bced7def5de41ef027e
gianmichelesiano <gianmichele.siano@gmail.com>
2021-03-20 07:07:47 +0100

    update custom

--- odoo-custom-addons/om_hospital/__manifest__.py
+++ odoo-custom-addons/om_hospital/__manifest__.py
@@ -1,35 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Academy",
+    'name': "Hospital Managmant",
+    'summary': """Modulo di prova da tutorial""",
+    'description': """Descrizione Modulo di prova da tutorial""",
+    'author': "Speats",
+    'website': "http://www.speats.ch",
+    'category': 'Extra Tools',
+    'version': '14.0.1.0.0',
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'data/sequence.xml',
+        'patient.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
